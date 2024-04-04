69a561e4c9821ce499e2b4817572c5894159af13
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-04-04 20:03:41 -0300

    feat: create new endpoint and add validator schema

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import fastify from 'fastify';
 import { ServerEnvs } from './config/envs';
+import { z } from 'zod';
 
 const app = fastify();
 
-app.get('/', () => {
+app.post('/events', (req, res) => {
+
+    const createEventSchema = z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable()
+    })
+
+    const data = createEventSchema.parse(req.body);
+
+    console.log(req.body);
+
     return 'Hello world';
 });
 
