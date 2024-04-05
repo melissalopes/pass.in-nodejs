fbd69cecaa7a9a019e7d37424b8d9393677152b2
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-04-05 11:50:13 -0300

    feat: improvement in the schema of the endpoint with provider zod

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,67 @@
 import fastify from 'fastify';
 import { ServerEnvs } from './config/envs';
 import { z } from 'zod';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod';
 import { PrismaClient } from '@prisma/client';
 import { generateSlug } from './utils/generate-slug';
 
 const app = fastify();
 
 const prisma = new PrismaClient({
-    log: ['query']
+  log: ['query'],
 });
 
-app.post('/events', async (req, res) => {
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-    const createEventSchema = z.object({
+app.withTypeProvider<ZodTypeProvider>().post(
+  '/events',
+  {
+    schema: {
+      body: z.object({
         title: z.string().min(4),
         details: z.string().nullable(),
-        maximumAttendees: z.number().int().positive().nullable()
-    })
-
-    const { title, details, maximumAttendees } = createEventSchema.parse(req.body);
+        maximumAttendees: z.number().int().positive().nullable(),
+      }),
+      response: {
+        201: z.object({
+          eventId: z.string().uuid(),
+        }),
+      },
+    },
+  },
+  async (req, res) => {
+    const { title, details, maximumAttendees } = req.body;
 
     const slug = generateSlug(title);
 
     const eventWithSameSlug = await prisma.event.findUnique({
-        where: {
-            slug,
-        }
-    })
+      where: {
+        slug,
+      },
+    });
 
-    if(eventWithSameSlug !==null){
-        throw new Error('Another event with same slug already exists!')
+    if (eventWithSameSlug !== null) {
+      throw new Error('Another event with same slug already exists!');
     }
 
     const event = await prisma.event.create({
-        data: {
-            title,
-            details,
-            maximumAttendees,
-            slug,
-        }
+      data: {
+        title,
+        details,
+        maximumAttendees,
+        slug,
+      },
     });
 
-    return res.status(201).send(event);
-});
+    return res.status(201).send({ eventId: event.id });
+  }
+);
 
-app.listen({ port: ServerEnvs.PORT}).then(() => console.log('HTTP server running!'));
+app
+  .listen({ port: ServerEnvs.PORT })
+  .then(() => console.log('HTTP server running!'));
