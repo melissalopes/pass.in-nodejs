aee63210142495f19addfc6cadfafb874f84c60d
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-04-05 18:04:43 -0300

    feat: improve return of 'get attendee badge'

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -4,34 +4,53 @@ import { prisma } from '../lib/prisma';
 import { FastifyInstance } from 'fastify';
 
 export async function getAttendeeBadge(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/attendee/:attendeeId/badge',
-    {
-      schema: {
-        params: z.object({
-            attendeeId: z.coerce.number().int(),
-          }),
-        response: {},
+    app.withTypeProvider<ZodTypeProvider>().get(
+        '/attendees/:attendeeId/badge',
+        {
+            schema: {
+                params: z.object({
+                    attendeeId: z.coerce.number().int(),
+                }),
+                response: {
+                    200: z.object({
+                        badge: z.object({
+                            name: z.string(),
+                            email: z.string().email(),
+                            eventTitle: z.string(),
+                        }),
+                    }),
+                },
+            },
         },
-      },
-    async (req, res) => {
-      const { attendeeId } = req.params;
+        async (req, res) => {
+            const { attendeeId } = req.params;
 
-      const attendee = await prisma.attendee.findUnique({
-        select: {
-            name: true,
-            email: true,
-        },
-        where: {
-            id: attendeeId
+            const attendee = await prisma.attendee.findUnique({
+                select: {
+                    name: true,
+                    email: true,
+                    event: {
+                        select: {
+                            title: true,
+                        },
+                    },
+                },
+                where: {
+                    id: attendeeId,
+                },
+            });
+
+            if (attendee === null) {
+                throw new Error('Attendee not found!');
+            }
+
+            return res.status(200).send({
+                badge: {
+                    name: attendee.name,
+                    email: attendee.email,
+                    eventTitle: attendee.event.title,
+                },
+            });
         }
-      })
-
-      if (attendee === null) {
-        throw new Error('Attendee not found!');
-      }
-
-      return res.status(200).send({ });
-    }
-  );
+    );
 }
